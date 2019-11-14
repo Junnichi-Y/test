5cdcdbbdd86d4acfe37e5a20ac188134275ba907
YOSHIJUN <64jun00zawayoshi8959@gmail.com>
2019-11-15 08:53:40 +0900

    Part8

--- order/order_download.php
+++ order/order_download.php
@@ -21,14 +21,20 @@
     <title>インテリア市場</title>
 </head>
 <body>
-    ショップ管理トップメニュー<br>
-    <br>
-    <a href="../staff/staff_list.php">スタッフ管理</a><br>
-    <br>
-    <a href="../product/pro_list.php">商品管理</a><br>
-    <br>
-    <a href="../order/order_download.php">注文ダウンロード</a><br>
-    <br>
-    <a href="staff_logout.php">ログアウト</a><br>
+    <?php
+        require_once('../common/common.php');
+    ?>
+
+    ダウンロードしたい注文日を選択して下しさい。<br>
+    <form action="order_download_done.php" method="post">
+        <?php pulldown_year(); ?>
+        年
+        <?php pulldown_month(); ?>
+        月
+        <?php pulldown_day(); ?>
+        日
+        <br><br>
+        <input type="submit" value="ダウンロード">
+    </form>
 </body>
 </html>
--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -58,6 +58,7 @@
 
                 $name=$rec['name'];
                 $price=$rec['price'];
+                $kakaku[]=$price;
                 $suryo=$kazu[$i];
                 $shokei=$price * $suryo;
 
@@ -66,43 +67,86 @@
                 $honbun.=$suryo.'個 = ';
                 $honbun.=$shokei."円\n";
             }
-        $dbh=null;
 
-        $honbun.="送料は無料です。\n";
-        $honbun.="--------------------\n";
-        $honbun.="代金は以下の口座にお振込みください。\n";
-        $honbun.="○○銀行　△△支店　普通口座　1234567 \n";
-        $honbun.="入金の確認が取れ次第、発送させていただきます。\n";
-        $honbun.="\n";
-        $honbun.="********************\n";
-        $honbun.="～会社名～\n";
-        $honbun.="\n";
-        $honbun.="AAA県BBB市CCC 123-4\n";
-        $honbun.="電話番号　090-xxxx-yyyy\n";
-        $honbun.="メールアドレス　abcdefg@vwxyz.co.jp\n";
-        $honbun.="********************\n";
-        //内容確認用
-        //'<br>';
-        //echo nl2br($honbun);
+            $sql = 'LOCK TABLES dat_sales WRITE,dat_sales_product WRITE';
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute();
 
-        $title='ご注文ありがとうございます。';
-        $header='From: abcdefg@vwxyz.co.jp';
-        $honbun=html_entity_decode($honbun, ENT_QUOTES, 'UTF-8');
-        mb_language('Japanese');
-        mb_internal_encoding('UTF-8');
-        mb_send_mail($email, $title, $honbun, $header);
+            $sql = 'INSERT INTO dat_sales(code_member,name,email,postal1,postal2,address,tel) VALUES(?,?,?,?,?,?,?)';
+            $stmt = $dbh->prepare($sql);
+            $data = array();
+            $data[]=0;
+            $data[]=$onamae;
+            $data[]=$email;
+            $data[]=$postal1;
+            $data[]=$postal2;
+            $data[]=$address;
+            $data[]=$tel;
+            $stmt->execute($data);
 
-        $title='お客様からご注文がありました。';
-        $header='From: '.$email;
-        $honbun=html_entity_decode($honbun, ENT_QUOTES, 'UTF-8');
-        mb_language('Japanese');
-        mb_internal_encoding('UTF-8');
-        mb_send_mail('abcdefg@vwxyz.co.jp', $title, $honbun, $header);
+            $sql = 'SELECT LAST_INSERT_ID()';
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute();
+            $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+            $lastcode = $rec['LAST_INSERT_ID()'];
+
+            for ($i=0; $i < $max; $i++) {
+                $sql='INSERT INTO dat_sales_product(code_sales,code_product,price,quantity) VALUES(?,?,?,?)';
+                $stmt=$dbh->prepare($sql);
+                $data=array();
+                $data[]=$lastcode;
+                $data[]=$cart[$i];
+                $data[]=$kakaku[$i];
+                $data[]=$kazu[$i];
+                $stmt->execute($data);
+            }
+
+            $sql = 'UNLOCK TABLES';
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute();
+
+            $dbh=null;
+
+            $honbun.="送料は無料です。\n";
+            $honbun.="--------------------\n";
+            $honbun.="代金は以下の口座にお振込みください。\n";
+            $honbun.="○○銀行　△△支店　普通口座　1234567 \n";
+            $honbun.="入金の確認が取れ次第、発送させていただきます。\n";
+            $honbun.="\n";
+            $honbun.="********************\n";
+            $honbun.="～会社名～\n";
+            $honbun.="\n";
+            $honbun.="AAA県BBB市CCC 123-4\n";
+            $honbun.="電話番号　090-xxxx-yyyy\n";
+            $honbun.="メールアドレス　abcdefg@vwxyz.co.jp\n";
+            $honbun.="********************\n";
+            //内容確認用
+            //'<br>';
+            //echo nl2br($honbun);
+
+            $title='ご注文ありがとうございます。';
+            $header='From: abcdefg@vwxyz.co.jp';
+            $honbun=html_entity_decode($honbun, ENT_QUOTES, 'UTF-8');
+            mb_language('Japanese');
+            mb_internal_encoding('UTF-8');
+            mb_send_mail($email, $title, $honbun, $header);
+
+            $title='お客様からご注文がありました。';
+            $header='From: '.$email;
+            $honbun=html_entity_decode($honbun, ENT_QUOTES, 'UTF-8');
+            mb_language('Japanese');
+            mb_internal_encoding('UTF-8');
+            mb_send_mail('abcdefg@vwxyz.co.jp', $title, $honbun, $header);
         }
         catch (Exception $e) {
             echo 'ただいま障害により大変ご迷惑をおかけしております。';
             exit();
         }
+        
+        $_SESSION=array();
+        session_destroy();
     ?>
+    <br>
+    <a href="shop_list.php">商品画面へ</a>
 </body>
 </html>
